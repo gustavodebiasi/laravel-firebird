3773e28a3532652249b8a83f51c550e62863aee7
OTIMIZY\gustavo <gustavo@otimizy.com.br>
2018-12-27 09:58:24 -0200

    Fix aggregate function in laravel - firebird paginate function

--- src/Firebird/Model.php
+++ src/Firebird/Model.php
@@ -1,7 +1,20 @@
 <?php namespace Firebird;
 
+use Firebird\Query\Builder;
 use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel {
+    /**
+	 * Get a new query builder instance for the connection.
+	 *
+	 * @return \Illuminate\Database\Query\Builder
+	 */
+	protected function newBaseQueryBuilder()
+	{
+		$connection = $this->getConnection();
 
+		return new Builder(
+			$connection, $connection->getQueryGrammar(), $connection->getPostProcessor()
+		);
+	}
 }
